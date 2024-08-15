7bb92a5500e640f47ac15a55a0cbb40b53d3a9cd
dgnc <1147422686@qq.com>
2024-08-15 17:47:16 +0800

    k

--- application/api/controller/upload/Upload.php
+++ application/api/controller/upload/Upload.php
@@ -33,20 +33,29 @@ class Upload extends Api
     public function list()
     {
         $params = $this->request->get();
+        $list = $this->list_item($params);
+        $this->success(__('Operation successful'), array(
+            'list' => $list
+        ));
+    }
+
+
+    public function list_item($params)
+    {
         $uploadClassifyId = !empty($params['upload_classify_id']) ? $params['upload_classify_id'] : -1;
         $status = !empty($params['status']) ? $params['status'] : -1;
-        $name =!empty( $params['name']) ? $params['name'] : '';
+        $name = !empty($params['name']) ? $params['name'] : '';
         $ids = !empty($params['ids']) ? $params['ids'] : '';
         $where = [];
         if ($status != -1) $where['status'] = $status;
-        if ($uploadClassifyId!= -1) $where['upload_classify_id'] = $uploadClassifyId;
-        if ($name) $where['name'] = ['like', '%'.$name.'%'];
+        if ($uploadClassifyId != -1) $where['upload_classify_id'] = $uploadClassifyId;
+        if ($name) $where['name'] = ['like', '%' . $name . '%'];
         if (!empty($ids)) {
-            $ids =  str_replace('，', ',', $ids);
+            $ids = str_replace('，', ',', $ids);
             $ids = explode(',', $ids);
             $where['id'] = ['in', $ids];
         }
-//	var_dump($where);exit;
+
         $list = Db::name('upload_file')->where($where)->select();
 
         foreach ($list as $key => &$item) {
@@ -59,25 +68,37 @@ class Upload extends Api
             $item['update_date'] = date('Y-m-d', $updatetime);
         }
 
+        return $list;
+    }
+
+    public function list_search()
+    {
+        $params = $this->request->get();
+        $config = Db::name('config')->where('name', '=', 'is_audit')->find();
+        if ($config && $config['value'] == 1) {
+            $params['ids'] = 27;
+        }
+        $list = $this->list_item($params);
         $this->success(__('Operation successful'), array(
-            'list'=> $list
+            'list' => $list
         ));
     }
 
-    public function getInfo(){
+    public function getInfo()
+    {
         $id = $this->request->get('id');
         if (empty($id)) $this->error(__('Parameter exception'));
         $info = db('upload_file')->where('id', $id)->find();
-        if (!$info)  $this->error(__('Parameter exception'));
-        if ($info['status'] != 1)  $this->error(__('Status exception'));
+        if (!$info) $this->error(__('Parameter exception'));
+        if ($info['status'] != 1) $this->error(__('Status exception'));
         $fileInfo = (new FileService())->getFileInfo([$info['local_url']])[0];
         $info['file_info_json'] = $fileInfo;
         $info['upload_classify_name'] = Db::name('upload_classify')->where('id', $info['upload_classify_id'])->value('name');
         $info['file_info_json']['size'] = 0;
-        $filePath = ROOT_PATH.'public'.$info['local_url'];
+        $filePath = ROOT_PATH . 'public' . $info['local_url'];
         if (file_exists($filePath)) {
             // 获取文件大小
-            $info['file_info_json']['size'] = round(filesize($filePath)  / 1024, 2);
+            $info['file_info_json']['size'] = round(filesize($filePath) / 1024, 2);
         }
         $createtime = $info['createtime'];
         $updatetime = $info['updatetime'];
@@ -94,7 +115,7 @@ class Upload extends Api
         $status = $this->request->get('status');
         if (empty($id) || empty($status)) $this->error(__('Parameter exception'));
         $info = db('upload_file')->where('id', $id)->find();
-        if (!$info)  $this->error(__('Parameter exception'));
+        if (!$info) $this->error(__('Parameter exception'));
 
         if ($status == 1) {
             Db::name('upload_file')->where('id', $id)->setInc('collect_count');
@@ -112,7 +133,7 @@ class Upload extends Api
         $id = $this->request->get('id');
         if (empty($id)) $this->error(__('Parameter exception'));
         $info = db('upload_file')->where('id', $id)->find();
-        if (!$info)  $this->error(__('Parameter exception'));
+        if (!$info) $this->error(__('Parameter exception'));
 
         Db::name('upload_file')->where('id', $id)->setInc('read_count');
         $this->success(__('Operation successful'));
